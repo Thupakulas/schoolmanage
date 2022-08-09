d4862f3bda1833491fadb0592de0001b39ec608d
Nagasai Thupakula <maltech.nagasai@gmail.com>
2022-08-09 17:35:05 +0530

    changes for index page along

--- index.php
+++ index.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>School Management</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>this is the landing page</h1>
+<?php
+
+include 'header.php';
+
+?>
+
+<img src="https://cdn.pixabay.com/photo/2016/12/11/08/01/coming-soon-1898936__340.jpg" width="100%" height="100%" alt="">
+    
 </body>
 </html>
